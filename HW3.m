f664cfabf23f4d7df18c178f79000a47e10dab55
JRenz8 <43185307+JRenz8@users.noreply.github.com>
2018-09-16 21:31:14 -0400

    Finished comments and fixed typos

--- HW3.m
+++ HW3.m
@@ -8,8 +8,8 @@ movie_rev_min = min(movie_rev); %Assigns the smallest value from the loaded HW3M
 movie_rev_min_index = find(movie_rev==movie_rev_min); %Finds index of smallest value in movie_rev_min.
 movie_rev_max = max(movie_rev); %Assigns the largest value from the loaded HW3Movies.dat file to movie_rev_max.
 movie_rev_max_index = find(movie_rev==movie_rev_max); %Finds index of largest value in movie_rev_max.
-disp("               ID   Revenue    " + newline... % These three lines (7-9)are one command split by the ... operator
-    + sprintf('Worst Movie:   %1$2d   $%2$-3.2g Mil', movie_rev_min_index,movie_rev_min)+ newline ...%They use the sprintf function to ensure that the table of best and worst movies would be formatted even if the data waas changed
+disp("               ID   Revenue    " + newline... % These three lines (7-9) are one command split by the ... operator.
+    + sprintf('Worst Movie:   %1$2d   $%2$-3.2g Mil', movie_rev_min_index,movie_rev_min)+ newline ...%The following uses the sprintf function to ensure that the table of best and worst movies would be formatted even if the data was changed.
     + sprintf('Best Movie:    %1$2d   $%2$-3.2g Mil', movie_rev_max_index,movie_rev_max))
 sorted_movie_rev= sort(movie_rev); %Assigns the action of sorting the HW3Movies.dat file to the variable sorted_movie_rev.
 disp(sorted_movie_rev); %Displays the value for the variable sorted_movie_rev in the command window.
@@ -17,15 +17,15 @@ disp(sorted_movie_rev); %Displays the value for the variable sorted_movie_rev in
 
 %------------Part 2-------------------------------------------------------%
 
-Matrix1 = load("HW3Matrix1.txt");%Loads in data from the file HW3Matrix1.txt into Matrix1
-Matrix2 = load("HW3Matrix2.txt");%Loads in data from the file HW3Matrix2.txt into Matrix2
-Sum_Matrix = Matrix1 + Matrix2;%Calculates and stores the sum of Matrix1 and Matrix2
-Product_Matrix = Matrix1 * Matrix2;%Calculates and stores the product of Matrix1 and Matrix2
-Element_Product_Matrix = Matrix1 .* Matrix2;%Calculates and stores the element product of Matrix1 and Matrix2
-disp("Sum of Matrix1 and Matrix2 :" + newline)%Dispalys the result of the above calcuations
-disp(Sum_Matrix)
-disp("Product of Matrix1 and Matrix2 :" + newline)
-disp(Product_Matrix)
-disp("Element Product of Matrix1 and Matrix2 :" + newline)
-disp(Element_Product_Matrix)
+Matrix1 = load("HW3Matrix1.txt"); %Loads in data from the file HW3Matrix1.txt into Matrix1.
+Matrix2 = load("HW3Matrix2.txt"); %Loads in data from the file HW3Matrix2.txt into Matrix2.
+Sum_Matrix = Matrix1 + Matrix2; %Calculates and stores the sum of Matrix1 and Matrix2.
+Product_Matrix = Matrix1 * Matrix2; %Calculates and stores the product of Matrix1 and Matrix2.
+Element_Product_Matrix = Matrix1 .* Matrix2; %Calculates and stores the element product of Matrix1 and Matrix2.
+disp("Sum of Matrix1 and Matrix2 :" + newline) %Presents the following title in the command window.
+disp(Sum_Matrix) %Displays the value for the variable Sum_Matrix in the command window.
+disp("Product of Matrix1 and Matrix2 :" + newline) %Presents the following title in the command window.
+disp(Product_Matrix) %Displays the value for the variable Product_Matrix in the command window.
+disp("Element Product of Matrix1 and Matrix2 :" + newline) %Presents the following title in the command window.
+disp(Element_Product_Matrix) %Displays the value for the variable Element_Product_Matrix in the command window.
 
